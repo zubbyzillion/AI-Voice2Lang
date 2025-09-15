ffea360b653cb18bb9521f4e446f2a40f5f98a9c
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2025-09-15 15:56:26 +0000

    Resolving deploy errors -3

--- src/types/react-speech-recognition.d.ts
+++ src/types/react-speech-recognition.d.ts
@@ -1,6 +1,14 @@
-// src/types/react-speech-recognition.d.ts
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 declare module "react-speech-recognition" {
   const SpeechRecognition: any
   export const useSpeechRecognition: any
   export default SpeechRecognition
+}
+
+// types/react-speech-recognition.d.ts
+declare module "react-speech-recognition" {
+  const SpeechRecognition: any;
+  export const useSpeechRecognition: any;
+  export default SpeechRecognition;
 }
